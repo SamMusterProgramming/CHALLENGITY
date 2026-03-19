e10872a7aab01f5c5766b57e2cfaf5e045da931f
samir <sam20232022@gmail.com>
2026-03-19 00:07:17 -0400

    fix dependencies, metro config, node version

--- metro.config.js
+++ metro.config.js
@@ -9,11 +9,19 @@
 
 
 
-const { getDefaultConfig } = require("@expo/metro-config");
+// const { getDefaultConfig } = require("@expo/metro-config");
+// const { withNativeWind } = require("nativewind/metro");
+// const path = require("path");
+
+// module.exports = withNativeWind(getDefaultConfig(__dirname), {
+//   input: "./global.css", // if you have a global Tailwind file
+// });
+
+const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const path = require("path");
 
-module.exports = withNativeWind(getDefaultConfig(__dirname), {
-  input: "./global.css", // if you have a global Tailwind file
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, {
+  input: "./global.css",
 });
-
